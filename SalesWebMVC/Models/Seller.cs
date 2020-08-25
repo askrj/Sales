8331925ccbd1b1b64e99bb9b944fb00451e5900b
askrj <33630041+askrj@users.noreply.github.com>
2020-08-25 09:29:44 -0300

    sedding service

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalari { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Departament Departament { get; set; }
+        public ICollection<SalesRecord> Sellers { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalari, DateTime birthDate, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalari = baseSalari;
+            BirthDate = birthDate;
+            Departament = departament;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sellers.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sellers.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime Final)
+        {
+            return Sellers.Where(sr => sr.Date >= initial && sr.Date <= Final).Sum(sr => sr.Amount);
+        }
+    }
+}
